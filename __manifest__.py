3f5e1441850c1584c125f9aed2c28305b9b981e2
Arfiyan19 <arfieyan1903@gmail.com>
2023-02-21 10:26:14 +0700

    push dev

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name" : "New Addons",
+    "version" : "1.0",
+    "author" : "Arfiyan Wahyu Pratama",
+    "website" : "https://www.github.com/Arfiyan19",
+    "category" : "New Module",
+    "summary" : "Ini adalah oddons baru",
+    "discription" : """
+        Latihan membuat addons baru di odoo 1
+        """,
+    "depends" : [
+        "base",
+    ],
+    "data" : [
+
+    ],
+    "demo" : [],
+    "test" : [],
+    "image" : [],
+    "qweb" :[],
+    "css" : [],
+    "application" : True,
+    "installable" : True,
+    "auto_install" : False,
+
+}
